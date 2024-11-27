870bccc470f43c9ca029f8201d13df5714770c08
Hana <enuxura@gmail.com>
2024-11-27 15:16:41 +0100

    build: add a `build.zig` at the root of the project and update the packages `build.zig` to be call be the root of the project

--- diagnostic/build.zig
+++ diagnostic/build.zig
@@ -1,28 +1,40 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+const utils = @import("../common_utils.zig");
 
-    const root_file = b.path("src/lib.zig");
+pub const version = std.SemanticVersion{
+    .major = 0,
+    .minor = 1,
+    .patch = 0,
+    .pre = "alpha",
+};
 
-    const lib = b.addStaticLibrary(.{
+pub fn build(b: *utils.Build) void {
+    const target = b.standard_target_options(.{ });
+    const optimize = b.standard_optimize_option(.{ });
+
+    const entry_file = b.path("src/lib.zig");
+
+    _ = b.add_module("diagnostic", .{
+        .root_source_file = entry_file,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const artifact_lib = b.add_library(.{
         .name = "diagnostic",
-        .root_source_file = root_file,
+        .root_source_file = entry_file,
         .target = target,
         .optimize = optimize,
+        .version = version,
     });
+    b.install_artifact(artifact_lib);
 
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = root_file,
+    b.add_test(.{
+        .name = "diagnostic-tests",
+        .root_source_file = entry_file,
         .target = target,
         .optimize = optimize,
+        .version = version,
     });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
