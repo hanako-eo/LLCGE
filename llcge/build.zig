78401eb7010759576fd0bd73e1a4f0f2d0ab76cc
Hana <enuxura@gmail.com>
2024-11-27 15:16:41 +0100

    build: add a `build.zig` at the root of the project and update the packages `build.zig` to be call be the root of the project

--- llcge/build.zig
+++ llcge/build.zig
@@ -1,41 +1,73 @@
 const std = @import("std");
 
-const version = std.SemanticVersion{
+const utils = @import("../common_utils.zig");
+
+pub const version = std.SemanticVersion{
     .major = 0,
     .minor = 1,
     .patch = 0,
     .pre = "alpha",
 };
 
-pub fn build(b: *std.Build) void {
-    const std_target = b.standardTargetOptions(.{});
-    const std_optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *utils.Build) void {
+    const target = b.standard_target_options(.{ });
+    const optimize = b.standard_optimize_option(.{ });
 
-    const root_file = b.path("src/lib.zig");
+    const entry_file = b.path("src/lib.zig");
 
-    //// BUILD LIB
-    _ = b.addModule("llcge", .{ .root_source_file = root_file });
+    _ = b.add_module("llcge", .{
+        .root_source_file = entry_file,
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "LLCGE",
-        .root_source_file = root_file,
-        .target = std_target,
-        .optimize = std_optimize,
+    const artifact_lib = b.add_library(.{
+        .name = "llcge",
+        .root_source_file = entry_file,
+        .target = target,
+        .optimize = optimize,
         .version = version,
     });
+    b.install_artifact(artifact_lib);
 
-    b.installArtifact(lib);
-
-    //// BUILD AND RUN TESTS
-    const test_step = b.step("test", "Run library tests");
-
-    const main_tests = b.addTest(.{
-        .root_source_file = root_file,
-        .target = std_target,
-        .optimize = std_optimize,
+    b.add_test(.{
+        .name = "llcge-tests",
+        .root_source_file = entry_file,
+        .target = target,
+        .optimize = optimize,
+        .version = version,
     });
-
-    const run_main_tests = b.addRunArtifact(main_tests);
-
-    test_step.dependOn(&run_main_tests.step);
 }
+
+// pub fn build(b: *std.Build) void {
+//     const std_target = b.standardTargetOptions(.{});
+//     const std_optimize = b.standardOptimizeOption(.{});
+
+//     const root_file = b.path("src/lib.zig");
+
+//     //// BUILD LIB
+//     _ = b.addModule("llcge", .{ .root_source_file = root_file });
+
+//     const lib = b.addStaticLibrary(.{
+//         .name = "llcge",
+//         .root_source_file = root_file,
+//         .target = std_target,
+//         .optimize = std_optimize,
+//         .version = version,
+//     });
+
+//     b.installArtifact(lib);
+
+//     //// BUILD AND RUN TESTS
+//     const test_step = b.step("test", "Run library tests");
+
+//     const main_tests = b.addTest(.{
+//         .name = "llcge-tests",
+//         .root_source_file = root_file,
+//         .target = std_target,
+//         .optimize = std_optimize,
+//     });
+
+//     const tests = b.addInstallArtifact(main_tests, .{});
+//     test_step.dependOn(&b.addRunArtifact(tests.artifact).step);
+// }
